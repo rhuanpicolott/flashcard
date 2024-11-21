77261c064b427379fc57665b0df712c3e9ac72d5
rhuanpicolott <00001104389824sp@al.educacao.sp.gov.br>
2024-11-21 09:47:13 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,26 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Charada',
+    'Dois pais e dois filhos voltam do supermercado. Mas, quando chegam em casa, apenas três pessoas saem do carro. Como isso é possível?',
+    'Eles são avô, pai e filho.',
+    'Há dois pais: o avô é pai do pai, o pai é pai do filho.',
+    'Há dois filhos: o pai é filho do avô, o filho é filho do pai.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Charada',
+    'A mãe de Mary teve quatro filhos. Abril, Maio e Junho foram os três primeiros. Qual o nome da 4ª criança?',
+    'Mary. A charada já deu essa informação no início.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
+    'charada',
+    'Quantas patas têm um casal de patos?',
+    'Têm 5, porque cada pato têm 2 patas (2 + 2 = 4) e somando com a pata (fêmea do casal), temos 5 patas.',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Charada',
+    'Eu pertenço ao mês de dezembro, e não a qualquer outro mês. Eu não sou um feriado. O que eu sou?',
+    'OA letra "d", porque nenhum outro mês tem essa letra.'
 )
